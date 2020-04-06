213ca180f8cd423704527360e069333d4f0bb78f
Ivan <ieroshenko@wpi.edu>
2020-04-06 14:37:53 -0400

    Possible open file fix

--- src/main/java/edu/wpi/N/Main.java
+++ src/main/java/edu/wpi/N/Main.java
@@ -1,21 +1,21 @@
 package edu.wpi.N;
 
+import java.io.File;
 import java.util.LinkedList;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    // specify path to .csv file
-    String pathToFile = Main.class.getResource("csv/MapCoordinates.csv").getPath();
+    // relative path to .csv file
+    File file = new File("src/main/resources/edu/wpi/N/csv/MapCoordinates.csv");
+    String pathToFile = file.getAbsolutePath();
 
     CSVParser parser = new CSVParser();
     Graph nodeGraph = parser.parseCSV(pathToFile);
-
     Node startNode = nodeGraph.getNode("MOHSClinic");
     Node endNode = nodeGraph.getNode("HVMANeurology");
     Pathfinder newPath = new Pathfinder(nodeGraph, startNode, endNode);
-
     LinkedList<Node> myPath = newPath.pathfinder();
 
     System.out.println("check");
